567837afdd6d561e957a23ce04f4dec2db0f93e0
huy ngo <vuhuy@inspectorio.com>
2018-09-16 00:37:49 +0700

    create class clock

--- clock/src/index.js
+++ clock/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Clock from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function tick () {
+    ReactDOM.render(<Clock date={new Date()}/>, document.getElementById('root'));
+
+}
+setInterval(tick,1000)
 registerServiceWorker();
