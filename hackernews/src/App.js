8d3f57660230a4db4be1c9304d24e50f5dc7e93c
huy ngo <vuhuy@inspectorio.com>
2018-09-17 00:10:53 +0700

    spit up component into components

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 
+// Create list data sample
 const list = [
     {
         title: 'React',
@@ -18,9 +19,58 @@ const list = [
         points: 5,
         objectId: 1,
     },
+    {
+        title: 'Jquery',
+        url: 'https://github.com/reactjs/jquery',
+        author: 'Huy Ngo',
+        num_comments: 4,
+        points: 7,
+        objectId: 4,
+    },
 ];
 
 const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class Search extends Component {
+    render() {
+        const {value, onChange} = this.props;
+        return (
+            <form>
+                <input type="text"
+                       value={value}
+                       onChange={onChange}
+                />
+            </form>
+        )
+    }
+}
+
+class Table extends Component {
+    render() {
+        const {list, pattern, onDismiss} = this.props;
+        return (
+            <div>
+                {list.filter(isSearched(pattern)).map(item =>
+                    <div key={item.objectId}>
+                    <span>
+                        <a href={item.url}>{item.title}</a>
+                    </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                        <span>
+                        <button
+                            onClick={() => onDismiss(item.objectId)}
+                            type='button'
+                        >
+                            Dismiss
+                        </button>
+                    </span>
+                    </div>)}
+            </div>
+        )
+    }
+}
 
 class App extends Component {
     constructor(props) {
@@ -34,58 +84,39 @@ class App extends Component {
 
         this.onSearchChange = this.onSearchChange.bind(this)
         this.onDismiss = this.onDismiss.bind(this);
-        this.onClickme = this.onClickme.bind(this);
-    }
-    onSearchChange(event){
-       // Set state
-        this.setState({searchTerm: event.target.value });
     }
 
-    onClickme(){
-        alert('Click me ahihihi');
+    onSearchChange(event) {
+        // Set state
+        this.setState({searchTerm: event.target.value});
     }
 
-    onDismiss(id){
+
+    onDismiss(id) {
         // When we dissmiss => get the new list without selected id, update this list again
 
         const isNotId = item => item.objectId !== id;
         const updatedList = this.state.list.filter(isNotId);
+        // when state was changed , render will be call
         this.setState({list: updatedList});
     }
 
     render() {
+        // firstly get searchTerm and list from current state
         const {searchTerm, list} = this.state
+        // after that pass them to another components
         return (
             <div className="App">
-                <form>
-                    <input value = { searchTerm} onChange={this.onSearchChange} name='name' type="text"/>
-                    <br/>
-                    <input type="age" type="text"/>
-                    <br/>
-                    <input type="gender" type="radio"/>
-
-                </form>
-                {list.filter(isSearched(searchTerm)).map(item => {
-                    return (
-                        <div key={item.objectId}>
-                            <span   onClick={console.log('you already click on title')}>
-                                <a href={item.url}>{item.title}</a>
-                            </span>
-                            <span onClick={this.onClickme}>{item.author}</span>
-                            <span>{item.num_comments}</span>
-                            <span>{item.points}</span>
-                            <span>
-                                <button
-                                    onClick={() => this.onDismiss(item.objectId)}
-                                >
-                                    Dismiss
-                                </button>
-                            </span>
-                            <h3>{this.state.searchTerm}</h3>
-                        </div>
-
-                    )
-                })} ;
+                <Search
+                    // pass parameter
+                    value={searchTerm}
+                    onChange={this.onSearchChange}
+                />
+                <Table
+                    list={list}
+                    pattern={searchTerm}
+                    onDismiss={this.onDismiss}
+                />
             </div>
         );
     }
